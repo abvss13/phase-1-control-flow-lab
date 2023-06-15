e82672e7cac71ed3406761752f8da9469f81d10e
Your Name <abdullahi.abass@student.moringschool.com>
2023-06-15 15:06:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet) {
+  if (distanceInFeet <= 400) {
+    return "This one is on me!";
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distanceInFeet > 2500) {
+    return "No can do.";
+  }
 }
