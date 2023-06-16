285e9266045238a79889bb4597a95632892c95a9
Your Name <abdullahi.abass@student.moringschool.com>
2023-06-16 11:33:46 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-function scuberGreetingForFeet(distanceInFeet) {
-  if (distanceInFeet <= 400) {
-    return "This one is on me!";
-  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
-    return "I will gladly take your thirty bucks.";
-  } else if (distanceInFeet > 2500) {
-    return "No can do.";
+function scuberGreetingForFeet(feet) {
+  let response;
+  if (feet <= 400) {
+    response = 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    response = 'No can do.';
+  }
+  return response;
+}
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
   }
 }
